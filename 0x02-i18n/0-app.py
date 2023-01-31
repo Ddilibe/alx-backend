35cadcb63e88c9f3d8a1e0d330616fbffc1bb878
Ddilibe <franklinfidelugwuowo@gmail.com>
2023-01-31 16:38:55 +0100

    [Chore]: Done with task 0
    A Basic Flask App

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -7,9 +7,11 @@ app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def index():
+def index() -> str:
+    """ Function for displaying the home page """
     return render_template('0-index.html')
 
 
 if __name__ == '__main__':
+    """ Running the Application """
     app.run(port='5000', host='0.0.0.0', debug=True)
