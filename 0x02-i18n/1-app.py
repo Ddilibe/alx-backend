a3d2861e17d5c1dcc32699af4c282ea8730cf013
Ddilibe <franklinfidelugwuowo@gmail.com>
2023-01-31 16:40:55 +0100

    [Chore]: Done with task 1
    A Basic Babel setup

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -5,18 +5,21 @@ from flask_babel import Babel
 
 
 class Config():
-	""" Class for Flask configuration """
-	LANGUAGES = ["en", "fr"]
-	BABEL_DEFAULT_LOCALE = "en"
-	BABEL_DEFAULT_TIMEZONE = 'UTC'
+    """ Class for Flask configuration """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
 babel = Babel(app)
 
+
 @app.route('/', strict_slashes=False)
 def index() -> render_template:
-	return render_template('0-index.html')
+    return render_template('0-index.html')
+
 
 if __name__ == '__main__':
-	app.run(port='5000', host='0.0.0.0', debug=True)
+    app.run(port='5000', host='0.0.0.0', debug=True)
