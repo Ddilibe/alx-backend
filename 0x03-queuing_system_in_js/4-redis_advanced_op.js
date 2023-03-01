2db2c1c97991cb867c27771ad93b94d58ab00c9b
Ddilibe <franklinfidelugwuowo@gmail.com>
2023-03-01 23:34:33 +0100

    [Chore]: Done with task 3
    [Chore]: Node redis client and advanced operations

---
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -0,0 +1,26 @@
+import {createClient, print} from "redis";
+import { promisify } from 'util';
+
+const client = createClient();
+
+client.on('connect', () => {
+    process.stdout.write("Redis client connected to the server\n");
+});
+client.on('error', (err) => {
+    process.stdout.write(`Redis client not connected to the server: ${err}\n`);
+});
+
+const keys = ["Portland", "Seattle", "New York", "Bogota", "Cali", "Paris"]
+const value = ["50", "80", "20", "20", "40","2"]
+const schoolName = "HolbertonSchools"
+
+for (var i = 0; i < keys.length; i++) {
+    client.hset(schoolName, keys[i], value[i], print);
+};
+client.hgetall(schoolName, (err, data) => {
+    if (err) {
+        process.stdout.write(`${err}\n`);
+        throw err;
+    }
+    process.stdout.write(`${data}\n`);
+})
