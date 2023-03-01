7bbc9f532158fa0ee35cc98de7425647c4fb9c9f
Ddilibe <franklinfidelugwuowo@gmail.com>
2023-03-02 00:35:15 +0100

    [Chore]: Done with task 6
    Create the Job creator

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,24 @@
+import {createQueue} from 'kue';
+
+const queue = createQueue();
+
+const notification = {
+  phoneNumber: "123456789",
+  message: "Learn to take care of your confessions",
+}
+
+const job = queue.create(
+    "push_notification_code",
+    notification
+).save((err) => {
+    if (!err) {
+        process.stdout.write(`Notification job created: ${job.id}\n`);
+    }
+});
+
+job.on('completed', () => {
+    process.stdout.write("Notification job complete\n");
+});
+job.on('failed', () => {
+    process.stdout.write("Notification job failed\n");
+});
